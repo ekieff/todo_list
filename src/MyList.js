4d9c18e3d9353459e153a09194e9520403b726ab
Elaine <elaine@Felipes-iMac.local>
2020-08-26 17:24:43 -0500

    [add constructors test console]

--- src/MyList.js
+++ src/MyList.js
@@ -3,6 +3,20 @@ import './App.css'
 import ListItem from './ListItem'
 
 class MyList extends Component {
+  
+  constructor(props){
+    super()
+    this.state = {
+      toDoItemArray: props.theList
+    }
+  }
+  clearList (e) {
+    console.log('clear')
+    this.setState({
+      toDoItemArray: []
+    })
+  }
+
   render() {
     let todoItems = this.props.theList.map((item, index) => (
       <ListItem doThis={item} key={index} />
@@ -13,6 +27,7 @@ class MyList extends Component {
         <ul>
           {todoItems} 
         </ul>
+        <button onClick= {(e) => this.clearList(e)}>Finished the list!</button>
       </div>
     )
   }
